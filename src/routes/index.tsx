0e79f7cbaa1e1644d85e816bdec15aff860b249d
Zaki <zhanghao670626@qq.com>
2024-03-11 02:21:57 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
+      <meta name="google-site-verification" content="vdnytIK1nn4cJvrh1OyTjhIqu3fVo6Vq0Nkj8g1q57E" />
       <h1>Hello world!</h1>
       <Counter />
       <p>
